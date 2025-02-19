3066e4fa1a849fe057b2cfe9568190f66ecd6707
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2025-02-18 22:54:16 -0300

    implementing subscribers repository

---
+++ src/model/repositories/subscribers_repository.py
@@ -0,0 +1,32 @@
+from src.model.configs.connection import DbConnectionHandler
+from src.model.entities.inscritos import Inscritos
+
+class SubscribersRepository:
+  def insert(self, subscribers_info: dict) -> None:
+    with DbConnectionHandler() as db:
+      try:
+        new_event = Inscritos(
+          nome=subscribers_info.get("name"),
+          email=subscribers_info.get("email"),
+          link=subscribers_info.get("link"),
+          evento_id=subscribers_info.get("evento_id"),
+        )
+        db.session.add(new_event)
+        db.session.commit()
+      except Exception as exception:
+        db.session.rollback()
+        raise exception
+
+  def select_subscriber(self, email: str, event_id: int) -> Inscritos:
+    with DbConnectionHandler() as db:
+      data = (
+        db.session
+          .query(Inscritos)
+          .filter(
+            Inscritos.email == email,
+            Inscritos.evento_id == event_id
+          )
+          .one_or_none()
+      )
+
+      return data
